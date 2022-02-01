493a2963d677ba572894d45f2aa5a4515203b84c
Vlad <vlad@ineract.com>
2022-02-01 11:25:13 -0500

    fix replacemenets

--- packages/ckeditor5-build-mergefield/src/mergeField/mergeFieldEditing.js
+++ packages/ckeditor5-build-mergefield/src/mergeField/mergeFieldEditing.js
@@ -1,111 +1,120 @@
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
 // MODIFIED
 import {
-  toWidget,
-  viewToModelPositionOutsideModelElement,
-} from "@ckeditor/ckeditor5-widget/src/utils";
-import Widget from "@ckeditor/ckeditor5-widget/src/widget";
+	toWidget,
+	viewToModelPositionOutsideModelElement,
+} from '@ckeditor/ckeditor5-widget/src/utils';
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 
-import MergeFieldCommand from "./mergeFieldCommand";
+import MergeFieldCommand from './mergeFieldCommand';
 
 export default class MergeFieldEditing extends Plugin {
-  static get requires() {
-    return [Widget];
-  }
-  init() {
-    this._defineSchema();
-    this._defineConverters();
+	static get requires() {
+		return [Widget];
+	}
+	init() {
+		this._defineSchema();
+		this._defineConverters();
 
-    this.editor.commands.add("mergefield", new MergeFieldCommand(this.editor));
+		this.editor.commands.add(
+			'mergefield',
+			new MergeFieldCommand(this.editor)
+		);
 
-    this.editor.editing.mapper.on(
-      "viewToModelPosition",
-      viewToModelPositionOutsideModelElement(this.editor.model, (viewElement) =>
-        viewElement.hasClass("mergefield")
-      )
-    );
+		this.editor.editing.mapper.on(
+			'viewToModelPosition',
+			viewToModelPositionOutsideModelElement(
+				this.editor.model,
+				(viewElement) => viewElement.hasClass('mergefield')
+			)
+		);
 
-    this.editor.config.define("mergeFieldsConfig", {
-      types: ["Name", "Date"],
-    });
-  }
+		this.editor.config.define('mergeFieldsConfig', {
+			types: ['Name', 'Date'],
+		});
+	}
 
-  _defineSchema() {
-    const schema = this.editor.model.schema;
+	_defineSchema() {
+		const schema = this.editor.model.schema;
 
-    schema.register("mergefield", {
-      // Allow wherever text is allowed:
-      allowWhere: "$text",
+		schema.register('mergefield', {
+			// Allow wherever text is allowed:
+			allowWhere: '$text',
 
-      // The merge field will act as an inline node:
-      isInline: true,
+			// The merge field will act as an inline node:
+			isInline: true,
 
-      // The inline widget is self-contained so it cannot be split by the caret and can be selected:
-      isObject: true,
+			// The inline widget is self-contained so it cannot be split by the caret and can be selected:
+			isObject: true,
 
-      // The inline widget can have the same attributes as text (for example linkHref, bold).
-      allowAttributesOf: "$text",
+			// The inline widget can have the same attributes as text (for example linkHref, bold).
+			allowAttributesOf: '$text',
 
-      // The merge fields can have many types, like date, name, surname, etc:
-      allowAttributes: ["name"],
-    });
-  }
+			// The merge fields can have many types, like date, name, surname, etc:
+			allowAttributes: ['name'],
+		});
+	}
 
-  _defineConverters() {
-    const conversion = this.editor.conversion;
+	_defineConverters() {
+		const conversion = this.editor.conversion;
 
-    conversion.for("upcast").elementToElement({
-      view: {
-        name: "span",
-        classes: ["mergefield"],
-      },
-      model: (viewElement, { writer: modelWriter }) => {
-        // Extract the "name" from "[#name]".
-        const name = viewElement.getChild(0).data.slice(2, -1);
+		conversion.for('upcast').elementToElement({
+			view: {
+				name: 'span',
+				classes: ['mergefield'],
+			},
+			model: (viewElement, { writer: modelWriter }) => {
+				// Extract the "name" from "[#name]".
+				const name = viewElement
+					.getChild(0)
+					.data.replace(/[\[#\]]+/g, '');
 
-        return modelWriter.createElement("mergefield", { name });
-      },
-    });
+				return modelWriter.createElement('mergefield', { name });
+			},
+		});
 
-    conversion.for("editingDowncast").elementToElement({
-      model: "mergefield",
-      view: (modelItem, { writer: viewWriter }) => {
-        const widgetElement = createMergeFieldView(modelItem, viewWriter);
+		conversion.for('editingDowncast').elementToElement({
+			model: 'mergefield',
+			view: (modelItem, { writer: viewWriter }) => {
+				const widgetElement = createMergeFieldView(
+					modelItem,
+					viewWriter
+				);
 
-        // Enable widget handling on a merge field element inside the editing view.
-        return toWidget(widgetElement, viewWriter);
-      },
-    });
+				// Enable widget handling on a merge field element inside the editing view.
+				return toWidget(widgetElement, viewWriter);
+			},
+		});
 
-    conversion.for("dataDowncast").elementToElement({
-      model: "mergefield",
-      view: (modelItem, { writer: viewWriter }) =>
-        createMergeFieldView(modelItem, viewWriter),
-    });
+		conversion.for('dataDowncast').elementToElement({
+			model: 'mergefield',
+			view: (modelItem, { writer: viewWriter }) =>
+				createMergeFieldView(modelItem, viewWriter),
+		});
 
-    // Helper method for both downcast converters.
-    function createMergeFieldView(modelItem, viewWriter) {
-      const name = modelItem.getAttribute("name");
+		// Helper method for both downcast converters.
+		function createMergeFieldView(modelItem, viewWriter) {
+			const name = modelItem.getAttribute('name');
 
-      const mergeFieldView = viewWriter.createContainerElement(
-        "span",
-        {
-          class: "mergefield",
-        },
-        {
-          isAllowedInsideAttributeElement: true,
-        }
-      );
+			const mergeFieldView = viewWriter.createContainerElement(
+				'span',
+				{
+					class: 'mergefield',
+				},
+				{
+					isAllowedInsideAttributeElement: true,
+				}
+			);
 
-      // Insert the merge field name (as a text).
-      const innerText = viewWriter.createText("[#" + name + "]");
-      viewWriter.insert(
-        viewWriter.createPositionAt(mergeFieldView, 0),
-        innerText
-      );
+			// Insert the merge field name (as a text).
+			const innerText = viewWriter.createText('[#' + name + ']');
+			viewWriter.insert(
+				viewWriter.createPositionAt(mergeFieldView, 0),
+				innerText
+			);
 
-      return mergeFieldView;
-    }
-  }
+			return mergeFieldView;
+		}
+	}
 }
